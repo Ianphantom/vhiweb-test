15cf8c2d35166585c5a5e662aa1a7818bedc54a2
Ianphantom <ian25yola@gmail.com>
2023-06-25 00:28:43 +0700

    ADD : Component Footer and Global Styles

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <FooterStyled className='text-w500 text-s14 text-grey'>
+      <div className='container'>
+        <div className='left'>
+          &#169; Copyright &#169; IanFelixJS. All Rights Reserved Copyright
+        </div>
+        <div className='right'>
+          <div>Terms and Conditions</div>
+          <div>Privacy Policy</div>
+          <div>Pricing</div>
+        </div>
+      </div>
+    </FooterStyled>
+  );
+};
+
+const FooterStyled = styled.div`
+  padding: 30px 0px;
+  border-top: 1px solid #eceef3;
+  .container {
+    display: flex;
+    justify-content: space-between;
+
+    .right {
+      display: flex;
+      gap: 20px;
+    }
+  }
+`;
+
+export default Footer;
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Inter', sans-serif;
+    }
+
+    a:hover{
+        list-style: none;
+        text-decoration: none;
+    }
+
+
+    .text-s14{
+        font-size: 14px;
+        line-height: 16px;
+    }
+
+    .text-s16{
+        font-size: 16px;
+        line-height: 16px;
+    }
+
+    .text-s24{
+        font-size: 24px;
+        line-height: 30px;
+    }
+
+    .text-w500{
+        font-weight: 500;
+    }
+
+    .text-w600{
+        font-weight: 600;
+    }
+
+    .text-w800{
+        font-weight: 800;
+    }
+
+    .text-grey{
+        color: #646A73;
+    }
+
+`;
+
+export default GlobalStyle;
