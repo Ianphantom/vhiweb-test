2376365844e1505f9531162745836e88c995e10d
Ianphantom <ian25yola@gmail.com>
2023-06-26 00:32:28 +0700

    EDIT : Responsive design at user list

--- src/pages/user/ColumnEmployeeName.js
+++ src/pages/user/ColumnEmployeeName.js
@@ -3,18 +3,33 @@ import styled from "styled-components";
 const ColumnEmployeeName = ({ avatar, first, last }) => {
   return (
     <ColumnStyled className='userImage'>
-      <img src={avatar} alt='UserPreview' />
-      {`${first} ${last}`}
+      <div>
+        <img src={avatar} alt='UserPreview' />
+      </div>
+      <div>{`${first} ${last}`}</div>
     </ColumnStyled>
   );
 };
 
 const ColumnStyled = styled.div`
+  display: flex;
+  align-items: center;
   img {
     height: 44px;
     width: 44px;
     border-radius: 50%;
     margin-right: 15px;
+  }
+
+  @media (max-width: 992px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    gap: 5px;
+    img {
+      margin-right: 0px;
+    }
   }
 `;
 export default ColumnEmployeeName;
